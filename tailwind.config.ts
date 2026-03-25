e83ff671c8d124f20cc49750a2f2f5d5a956cfb9
Patcharapong Chuanchom <Patcharapong@charnissara.local>
2026-03-25 11:17:40 +0700

    fix tailwind config content paths to prevent css purging

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,18 +4,10 @@ const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}", // 👈 บรรทัดนี้สำคัญที่สุด! มันคือการสั่งให้สแกนโฟลเดอร์ app
   ],
   theme: {
-    extend: {
-      colors: {
-        tennis: '#CCFF00', // สีเขียวมะนาวเทนนิส
-      },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
